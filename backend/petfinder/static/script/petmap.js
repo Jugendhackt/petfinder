c74a335a4f343d32364f4fe2c7c7e4a1ef8213ca
pajowu <github@ca.pajowu.de>
2014-09-14 04:01:01 +0200

    Users can now register (no QR, just adding the pet to the db)

--- backend/petfinder/static/script/petmap.js
+++ backend/petfinder/static/script/petmap.js
@@ -55,8 +55,12 @@ function parseResponsePets(data) {
     L.geoJson(data, {
       onEachFeature: function (feature, layer) {
         console.log(feature)
-
-        layer.setIcon(L.icon({iconSize: [48,48],iconAnchor: [24,48],popupAnchor:[0,-48],iconUrl: "images/" + feature.properties.type  +".svg"}))
+        animals = ["Katze","Hund","Vogel","Hase"]
+        type = feature.properties.type;
+        if(not type in animals) {
+          type = "undef"
+        }
+        layer.setIcon(L.icon({iconSize: [48,48],iconAnchor: [24,48],popupAnchor:[0,-48],iconUrl: "images/" + type  +".svg"}))
         //layer.bindPopup("<p><img width=100% src='" + feature.value.image + "'>" + feature.value.title + "<a href=https://ingress.com/intel?ll=" + feature.value.latE6/1E6 + "," + feature.value.lngE6/1E6 + "&z=17&pll=" + feature.value.latE6/1E6 + "," + feature.value.lngE6/1E6 +">Open Portal on Intel</a></p>");
         popupsting = "<p>Name: " + feature.properties.name + "</p>";
         if('type' in feature.properties){
--- backend/petfinder/static/script/register.js
+++ backend/petfinder/static/script/register.js
@@ -1,3 +1,31 @@
+var data = {}
+var map = L.map('map');
+console.log(map)
+var url = 'http://{s}.tiles.mapbox.com/v3/pajowu.jg9he75o/{z}/{x}/{y}.png';
+var copyright = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
+var tileLayer = new L.TileLayer(url, {
+  maxZoom: 20,
+  attribution: copyright
+});
+var startPosition = new L.LatLng(52.5306525,13.4135768);
+map.setView(startPosition, 15)
+.addLayer(tileLayer);
 function register(){
-    console.log("register")
+    data['name'] = document.getElementById("aniname").value
+    data['age'] = document.getElementById("aniage").value
+    data['attributes'] = document.getElementById("aniatt").value
+    console.log(data)
+    $.ajax({
+        type: 'POST',
+        url: '/pets',
+        data: JSON.stringify({"pet":data}),
+        success: function(data) {
+            data = JSON.parse(data)
+            console.log(data)
+        }});
 }
+function setAnimal(animtype){
+    document.getElementById("animtypedrop").firstChild.data = animtype
+    data['type'] = animtype
+}
+function onClick(e) {alert(this.getLatLng());}
